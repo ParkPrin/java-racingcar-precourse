6566a7d19229d40efeb26febd5b82eec2ea54b88
박종훈 <m05214@naver.com>
2022-04-23 13:37:50 +0900

    Feat: RacingCar 내부 유효성 검사 추가
    RacingCar 생성시 유효성검사 하는 로직 추가함

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -1,11 +1,13 @@
 package racingcar.domain;
 
+import org.junit.platform.commons.util.StringUtils;
+
 public class RacingCar {
     final private String name;
     private int travel;
 
     public RacingCar(String name){
-        this.name = name;
+        this.name = racingCarCreateValidation(name);
         this.travel = 0;
     }
 
@@ -19,5 +21,21 @@ public class RacingCar {
 
     public void raingDistance(int travel) {
         this.travel = this.travel + travel;
+    }
+
+    /**
+     * 자동차 이름의 유효성검사
+     * @param name
+     * @return
+     */
+    private String racingCarCreateValidation(String name){
+        // 자동차 이름이 존재하지 않는 경우
+        if (StringUtils.isBlank(name)){
+            throw new IllegalArgumentException("[ERROR] 자동차 이름을 입력하세요");
+        }
+        if (name.length() > 5){
+            throw new IllegalArgumentException("[ERROR] 자동차 이름은 5자 이하만 가능합니다");
+        }
+        return name;
     }
 }
