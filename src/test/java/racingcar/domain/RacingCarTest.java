16f0bf1a1ea5f9e8f226b006209d79da7ad1b873
박종훈 <m05214@naver.com>
2022-04-23 13:58:01 +0900

    Test: 레이싱카 객체 생성 테스트
    레이싱카 객체 생성시 실패(이름없음), 실패(이름5자초과), 성공 케이스로 테스트코드 작성함
    
    Resolves: #RP_2

---
+++ src/test/java/racingcar/domain/RacingCarTest.java
@@ -0,0 +1,56 @@
+package racingcar.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RacingCarTest {
+
+    @DisplayName("레이싱카 생성 테스트 - 실패(이름없음)")
+    @Test
+    void createRacingCarValidationTestToFailAtNullName(){
+        // given
+        String name = "";
+
+        // when & then
+        org.assertj.core.api.Assertions.assertThatThrownBy(
+                () -> {
+                    RacingCar racingcar = new RacingCar(name)            ;
+                }
+        )
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 자동차 이름을 입력하세요");
+    }
+
+    @DisplayName("레이싱카 생성 테스트 - 실패(이름5자초과)")
+    @Test
+    void createRacingCarValidationTestToFailAtOver5Name(){
+        // given
+        String name = "racingCar";
+
+        // when & then
+        org.assertj.core.api.Assertions.assertThatThrownBy(
+                () -> {
+                    RacingCar racingcar = new RacingCar(name)            ;
+                }
+        )
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 자동차 이름은 5자 이하만 가능합니다");
+    }
+
+    @DisplayName("레이싱카 생성 테스트 - 성공")
+    @Test
+    void createRacingCarValidationTestToSuccess(){
+        // given
+        String name = "jimi";
+
+        // when
+        RacingCar racingcar = new RacingCar(name);
+
+        // then
+        assertEquals(racingcar.getName(), name);
+
+    }
+
+}
